b1fd89cf55f785b5226497f01c1ed9f334da5b00
VoltVisionFrenchy <github@voltvision.com>
2013-11-23 23:04:38 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var Sensor = require('./lib/sensor'),
+	Sound = require('./lib/sound');
+
+var sensor = new Sensor({
+	threshold : 0.47,
+	intervalRate : 10
+});
+
+var loop = new Sound({
+	path : "./sounds/loop.wav"
+});
+
+sensor.start();
+
+sensor.on('threshold:over', function (data) {
+	console.log('threshold over', data);
+});
+
+sensor.on('threshold:under', function (data) {
+	console.log('threshold under', data);
+	loop.play();
+});
+
+console.log('Sensor running');
